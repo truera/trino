5e52371c80317183b76cd4b28885d8fb5053ae25
abhay-kc-truera <abhay@truera.com>
2023-08-22 15:31:43 +0530

    add install_plugin_local.sh

---
+++ plugin/trino-truera/install_plugin_local.sh
@@ -0,0 +1,8 @@
+mvn clean install -Dair.check.skip-all=true -DskipTests
+if [ -z "${AILENS_DEPENDENCIES_DIR_OVERRIDE}" ]
+then
+    echo "Expected AILENS_DEPENDENCIES_DIR_OVERRIDE."
+    exit 1
+fi
+rm -rf $AILENS_DEPENDENCIES_DIR_OVERRIDE/trino-server/plugin/trino-truera*
+unzip target/trino-truera-*.zip -d $AILENS_DEPENDENCIES_DIR_OVERRIDE/trino-server/plugin
