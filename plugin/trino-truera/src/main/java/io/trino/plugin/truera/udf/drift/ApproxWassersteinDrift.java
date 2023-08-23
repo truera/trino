adebce21c93a67aa650a7619ec1035905f959ee5
abhay-kc-truera <abhay@truera.com>
2023-08-23 23:33:54 +0530

    add default accuracy in approx_wasserstein_drift

--- plugin/trino-truera/src/main/java/io/trino/plugin/truera/udf/drift/ApproxWassersteinDrift.java
+++ plugin/trino-truera/src/main/java/io/trino/plugin/truera/udf/drift/ApproxWassersteinDrift.java
@@ -1,32 +1,41 @@
 package io.trino.plugin.truera.udf.drift;
 
-import com.clearspring.analytics.stream.quantile.QDigest;
 import com.google.common.base.Preconditions;
 import io.airlift.slice.Slice;
 import io.trino.spi.function.Description;
 import io.trino.spi.function.ScalarFunction;
-import io.trino.spi.function.SqlNullable;
 import io.trino.spi.function.SqlType;
 import io.trino.spi.type.StandardTypes;
 
 import java.util.Arrays;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 import static io.trino.operator.aggregation.FloatingPointBitsConverterUtil.sortableLongToDouble;
+import static io.trino.operator.scalar.QuantileDigestFunctions.DEFAULT_ACCURACY;
 import static io.trino.operator.scalar.QuantileDigestFunctions.valueAtQuantileBigint;
 
 public class ApproxWassersteinDrift {
 
-    private ApproxWassersteinDrift(){}
+    public static final double ACCURACY = 0.001;
 
     @ScalarFunction(value="approx_wasserstein_drift", deterministic = true)
     @Description("Returns ApproxWassersteinDrift between 2 sketches")
-    @SqlType(StandardTypes.DOUBLE)
-    public static double WassersteinDrift(
+    @SqlType(StandardTypes.BIGINT)
+    public static double approxWassersteinDrift(
             @SqlType("qdigest(double)") Slice leftSketch,
-            @SqlType("qdigest(double)") Slice rightSketch)
-    {
-        final double[] fractions = getBinsFromRange(0., 1., 1000);
+            @SqlType("qdigest(double)") Slice rightSketch) {
+        return approxWassersteinDrift(leftSketch,rightSketch, DEFAULT_ACCURACY);
+    }
+
+    @ScalarFunction(value="approx_wasserstein_drift", deterministic = true)
+    @Description("Returns ApproxWassersteinDrift between 2 sketches")
+    @SqlType(StandardTypes.BIGINT)
+    public static double approxWassersteinDrift(
+            @SqlType("qdigest(double)") Slice leftSketch,
+            @SqlType("qdigest(double)") Slice rightSketch,
+            @SqlType(StandardTypes.DOUBLE) double accuracy) {
+        final double[] fractions = getBinsFromRange(0., 1., (long) (1/accuracy));
         final double[] leftQuantiles = Arrays.stream(fractions).map(
                 quantile -> sortableLongToDouble(valueAtQuantileBigint(leftSketch, quantile))
         ).toArray();
@@ -50,7 +59,7 @@ public class ApproxWassersteinDrift {
         return area;
     }
 
-    public static double[] getBinsFromRange(double start, double stop, int numBins) {
-        return IntStream.rangeClosed(0, numBins).mapToDouble(i -> start + i * ((stop - start) / (float) numBins)).toArray();
+    public static double[] getBinsFromRange(double start, double stop, long numBins) {
+        return LongStream.rangeClosed(0, numBins).mapToDouble(i -> start + i * ((stop - start) / (float) numBins)).toArray();
     }
 }
