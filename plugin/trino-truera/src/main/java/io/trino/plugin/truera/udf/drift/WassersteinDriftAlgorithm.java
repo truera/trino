2e779361f56dc2887fe5201b9544eb042764521e
abhay-kc-truera <abhay@truera.com>
2023-08-23 23:09:26 +0530

    remove non scikit compliant implementation

--- plugin/trino-truera/src/main/java/io/trino/plugin/truera/udf/drift/WassersteinDriftAlgorithm.java
+++ plugin/trino-truera/src/main/java/io/trino/plugin/truera/udf/drift/WassersteinDriftAlgorithm.java
@@ -18,37 +18,6 @@ public class WassersteinDriftAlgorithm {
     private static final double FRACTIONS = 1000;
 
     /**
-     * Returns wasserstein distance between two lists using trapezoidal integration.
-     * This approach requires using percentile objects to create CDF.
-     */
-    public static double computeWasserstein(List<Double> leftSeries, List<Double> rightSeries) {
-        double[] leftValues = leftSeries.stream().sorted().mapToDouble(Double::doubleValue).toArray();
-        double[] rightValues = rightSeries.stream().sorted().mapToDouble(Double::doubleValue).toArray();
-
-        final Percentile percentile = new Percentile();
-        final List<Double> leftQuantileValues = IntStream.range(0,1000).mapToDouble(
-                i -> {
-                    double queryPercentile = i==0? EPSILON : i*100.0/FRACTIONS;
-                    return percentile.evaluate(leftValues, queryPercentile);
-                }
-        ).boxed().toList();
-        final List<Double> rightQuantileValues = IntStream.range(0,1000).mapToDouble(
-                i -> {
-                    double queryPercentile = i==0? EPSILON : i*100.0/FRACTIONS;
-                    return percentile.evaluate(rightValues, queryPercentile);
-                }
-        ).boxed().toList();
-        double result = 0.0;
-        for(int i=1;i<FRACTIONS-1;i++){
-            final double side1 = Math.abs(leftQuantileValues.get(i) - rightQuantileValues.get(i));
-            final double side2 = Math.abs(leftQuantileValues.get(i+1) - rightQuantileValues.get(i+1));
-            result += 0.5 * (side1 + side2) * 1.0/FRACTIONS;
-            LOGGER.info(side1 + ":" + side2 + ":" + result);
-        }
-        return result;
-    }
-
-    /**
      * Returns wasserstein distance between two lists using the same approach as scikit learn.
      * <a href="https://github.com/scipy/scipy/blob/v1.11.2/scipy/stats/_stats_py.py#L9896">Scikit Source.</a>
      */
