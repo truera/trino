8bd4cf33bbf8db7edcc3999dd9ad33f76c4d0eeb
abhay-kc-truera <abhay@truera.com>
2023-08-24 14:22:05 +0530

    optimise compute cdf implementation

--- plugin/trino-truera/src/main/java/io/trino/plugin/truera/udf/drift/WassersteinDriftAlgorithm.java
+++ plugin/trino-truera/src/main/java/io/trino/plugin/truera/udf/drift/WassersteinDriftAlgorithm.java
@@ -1,58 +1,72 @@
 package io.trino.plugin.truera.udf.drift;
 
-import org.apache.commons.math3.stat.descriptive.rank.Percentile;
-
+import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class WassersteinDriftAlgorithm {
-
-    private static final  Logger LOGGER = Logger.getLogger(WassersteinDriftAlgorithm.class.getName());
-
-    private static final double EPSILON = 0.001;
-    private static final double FRACTIONS = 1000;
 
     /**
      * Returns wasserstein distance between two lists using the same approach as scikit learn.
      * <a href="https://github.com/scipy/scipy/blob/v1.11.2/scipy/stats/_stats_py.py#L9896">Scikit Source.</a>
      */
     public static double computeWassersteinScikit(List<Double> leftSeries, List<Double> rightSeries) {
+        long lastTime = Instant.now().toEpochMilli();
         List<Double> allSeries = new ArrayList<>(leftSeries);
         allSeries.addAll(rightSeries);
         Collections.sort(allSeries);
-        final List<Double> leftSorted = leftSeries.stream().sorted().toList();
-        final List<Double> rightSorted = rightSeries.stream().sorted().toList();
-
-        final List<Double> leftCdf = allSeries.subList(0, allSeries.size()-1)
-                .stream().map( v -> searchInsertIndexFromRight(leftSorted,v)*1.0/leftSorted.size()).toList();
-        final List<Double> rightCdf = allSeries.subList(0, allSeries.size()-1)
-                .stream().map( v -> searchInsertIndexFromRight(rightSorted,v)*1.0/rightSorted.size()).toList();
+        final List<Double> leftSorted = new ArrayList<>(leftSeries);
+        final List<Double> rightSorted = new ArrayList<>(rightSeries);
+        Collections.sort(leftSorted);
+        Collections.sort(rightSorted);
+        // TODO: Use non global logger
+        Logger.getGlobal().info("Time taken to run sort all lists: "+(Instant.now().toEpochMilli()-lastTime));
+        lastTime = Instant.now().toEpochMilli();
+        final double[] leftCdf = buildCdf(leftSorted,allSeries);
+        final double[] rightCdf = buildCdf(rightSorted,allSeries);
+        Logger.getGlobal().info("Time taken to run cdf creation: "+(Instant.now().toEpochMilli()-lastTime));
+        lastTime = Instant.now().toEpochMilli();
         final List<Double> deltas = IntStream.range(0, allSeries.size()-1)
                 .mapToDouble(i -> allSeries.get(i+1) - allSeries.get(i))
                 .boxed().toList();
         double result = 0.0;
         for(int i=0;i<deltas.size();i++){
-            result += Math.abs(leftCdf.get(i) - rightCdf.get(i)) * deltas.get(i);
+            result += Math.abs(leftCdf[i] - rightCdf[i]) * deltas.get(i);
         }
+        Logger.getGlobal().info("Time taken to complete compute drift: "+ (Instant.now().toEpochMilli()-lastTime));
         return result;
     }
 
-    public static int searchInsertIndexFromRight(List<Double> list, double value) {
-        int insertIndex = Collections.binarySearch(list,value);
-        int maxRightIndex = insertIndex;
-        if(insertIndex < 0) {
-            maxRightIndex = -insertIndex - 1;
-        }
-        for(int i=maxRightIndex; i<list.size();i++) {
-            if (list.get(i).equals(value)){
-                maxRightIndex+=1;
+    /**
+     * Similar to np.searchsorted([], 'right')
+     * for each element in allList find the right most index i of insertion in partialList such that,
+     * partialList[j] <= element for all 0<=j<=i
+     */
+    public static double[] buildCdf(List<Double> partialList, List<Double> allList) {
+        int[] indices = new int[allList.size()-1];
+        int prevIndex = 0;
+        double prevValue = Double.MIN_VALUE;
+        for(int i=0;i<allList.size()-1;i++){
+            double item = allList.get(i);
+            if(prevValue == item){
+                indices[i] = prevIndex;
+                continue;
             }
+            int newIndex = prevIndex;
+            while(newIndex<partialList.size() && partialList.get(newIndex)<=item){
+                newIndex += 1;
+            }
+            prevValue = item;
+            prevIndex = newIndex;
+            indices[i] = newIndex;
         }
-        return maxRightIndex;
+        double[] cdf = new double[indices.length];
+        for(int i=0;i<indices.length;i++){
+            cdf[i] = indices[i]*1.0/partialList.size();
+        }
+        return cdf;
     }
 }
